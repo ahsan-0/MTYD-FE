b424257f9c4613471e85856cd0f0a9ae7b2e0a41
sam petcher <sampetcher@gmail.com>
2023-01-27 15:08:50 +0000

    shinify table

--- MTYD-FE/src/components/Table.jsx
+++ MTYD-FE/src/components/Table.jsx
@@ -1,34 +1,50 @@
-
+import img from "./metal.png";
+import * as THREE from "three";
+const texture = new THREE.TextureLoader().load(img);
 function Table({ size }) {
-    return (
-        <>
-        <mesh position={[(size[size.length - 1].coords[0] / 2), -1, (size[size.length - 1].coords[0] / 2)]}>
-            <meshStandardMaterial />
-            <boxGeometry args={
-                [size[size.length - 1].coords[0] + 3,
-                1,
-                size[size.length - 1].coords[0] + 3
-                ]}/>   
-        </mesh>
-        <mesh>
-          <meshStandardMaterial />
-          <boxGeometry args={[]}/>
-        </mesh>
+  return (
+    <>
+      <mesh
+        position={[
+          size[size.length - 1].coords[0] / 2,
+          -1,
+          size[size.length - 1].coords[0] / 2,
+        ]}
+      >
+        <meshMatcapMaterial
+          opacity={1}
+          depthTest={true}
+          depthWrite={true}
+          alphaTest={0}
+          color={"white"}
+          matcap={texture}
+        />
+        <boxGeometry
+          args={[
+            size[size.length - 1].coords[0] + 3,
+            1,
+            size[size.length - 1].coords[0] + 3,
+          ]}
+        />
+      </mesh>
+      <mesh>
+        <meshStandardMaterial />
+        <boxGeometry args={[]} />
+      </mesh>
 
-        <mesh>
-          <meshStandardMaterial />
-          <boxGeometry />
-        </mesh>
+      <mesh>
+        <meshStandardMaterial />
+        <boxGeometry />
+      </mesh>
 
-        <mesh>
-          <meshStandardMaterial />
-          <boxGeometry />
-        </mesh>
-        </>
-    );
+      <mesh>
+        <meshStandardMaterial />
+        <boxGeometry />
+      </mesh>
+    </>
+  );
 }
 export default Table;
-
 
 //  <boxGeometry args={[length, width, height]}/>
 
