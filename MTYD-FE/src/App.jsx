90e0fed211344434b183bd86b9362c982c99964d
sam petcher <sampetcher@gmail.com>
2023-01-27 10:49:00 +0000

    Scene based changes

--- MTYD-FE/src/App.jsx
+++ MTYD-FE/src/App.jsx
@@ -2,22 +2,38 @@ import { useState } from "react";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import { Camera } from "three";
-import { CameraControls } from "@react-three/drei";
+import { CameraControls, Sky, Sparkles } from "@react-three/drei";
+import LiveCell from "./components/LiveCell";
+import DeadCell from "./components/DeadCell";
+import PlayArea from "./components/PlayArea";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Canvas>
+        <Sparkles {...props} />
+        <Sky exposure={0.01} elevation={0.01} azimuth={90} rayleigh={0} />
         <CameraControls />
-        <mesh>
-          <boxGeometry />
-          <meshBasicMaterial />
-        </mesh>
+        <PlayArea />
       </Canvas>
     </div>
   );
 }
 
 export default App;
+
+const props = {
+  /** Number of particles (default: 100) */ count: 10000,
+  /** Speed of particles (default: 1) */
+  speed: 1,
+  /** Opacity of particles (default: 1) */
+  opacity: 1,
+  /** Color of particles (default: 100) */
+  color: 100,
+  /** Size of particles (default: randomized between 0 and 1) */
+  size: 5,
+  /** The space the particles occupy (default: 1) */
+  scale: [100, 100, 100],
+  /** Movement factor (default: 1) */
+  noise: 1,
+};
