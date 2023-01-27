78efecf07316fa2a3068f3ea44c50c8f33f55e7f
3782291211 <Laklouk98@hotmail.com>
2023-01-27 14:22:30 +0000

    Adding meshes to Table component

--- MTYD-FE/src/components/Table.jsx
+++ MTYD-FE/src/components/Table.jsx
@@ -1,11 +1,36 @@
 
 function Table({ size }) {
-    console.log(size);
     return (
-        <mesh >
+        <>
+        <mesh position={[(size[size.length - 1].coords[0] / 2), -1, (size[size.length - 1].coords[0] / 2)]}>
             <meshStandardMaterial />
-            <boxGeometry />
+            <boxGeometry args={
+                [size[size.length - 1].coords[0] + 3,
+                1,
+                size[size.length - 1].coords[0] + 3
+                ]}/>   
         </mesh>
+        <mesh>
+          <meshStandardMaterial />
+          <boxGeometry args={[]}/>
+        </mesh>
+
+        <mesh>
+          <meshStandardMaterial />
+          <boxGeometry />
+        </mesh>
+
+        <mesh>
+          <meshStandardMaterial />
+          <boxGeometry />
+        </mesh>
+        </>
     );
 }
 export default Table;
+
+
+//  <boxGeometry args={[length, width, height]}/>
+
+//turn the table into a flat plane
+//set the table size to be the same as all of the cells
