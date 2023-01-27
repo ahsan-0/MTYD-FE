9cdd7aead05b871cc3f48a8f5b46e5e64cbe041e
3782291211 <Laklouk98@hotmail.com>
2023-01-27 14:20:52 +0000

    Assigning key props

--- MTYD-FE/src/components/PlayArea.jsx
+++ MTYD-FE/src/components/PlayArea.jsx
@@ -2,27 +2,28 @@ import Table from "./Table";
 import Cell from "./Cell";
 import { boardData } from "./board.json";
 import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 function PlayArea() {
-    
   const [boardConfiguration, setBoardConfiguration] = useState([]);
 
-    useEffect( () => {
-  const cellCoords = [];
-  for (let i = 0; i < boardData.length; i++) {
-    for (let j = 0; j < boardData[i].length; j++) {
-      cellCoords.push({ coords: [i, 0, j], alive: boardData[i][j] });
+  useEffect( () => {
+    const cellCoords = [];
+    for (let i = 0; i < boardData.length; i++) {
+      for (let j = 0; j < boardData[i].length; j++) {
+        cellCoords.push({ coords: [i, 0, j], alive: boardData[i][j] });
+      }
     }
-  }
-  setBoardConfiguration(cellCoords);
-},[])
+    setBoardConfiguration(cellCoords);
+  },[]);
+  
   return (
-    <>
-  {boardConfiguration.map((cell) => (
-    <Cell position={cell.coords} living={cell.alive} />
-  ))}
-      <Table size={boardConfiguration} />
-    </>
+  <>
+    {boardConfiguration.map((cell) => (
+    <Cell key={uuidv4()} position={cell.coords} living={cell.alive} />
+    ))}
+    {boardConfiguration.length != 0 && <Table size={boardConfiguration} />}
+  </>
   );
 }
 export default PlayArea;
