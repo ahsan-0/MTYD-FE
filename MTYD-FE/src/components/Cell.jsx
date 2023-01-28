2e0be24f595b981a322d33e64fb5cadf9c8f1740
3782291211 <Laklouk98@hotmail.com>
2023-01-28 01:45:25 +0000

    Updating mesh colors on cell component

--- MTYD-FE/src/components/Cell.jsx
+++ MTYD-FE/src/components/Cell.jsx
@@ -1,14 +1,23 @@
+import { Bloom } from "@react-three/postprocessing";
+import { Edges } from "@react-three/drei"
 
 function Cell({ position, living }) {
     return (
-        <mesh position={position}>
-            {  living === 1 ?
-            <meshStandardMaterial emissive="yellow" emissiveIntensity={2} transparent toneMapped={false} opacity={0.5} />
-            :
-            <meshStandardMaterial transparent opacity={0.1} />
-            }
+        <mesh onClick={() => console.log(position)} position={position}>
+            { living === 1 ?
+            <><meshStandardMaterial emissive="hotpink" emissiveIntensity={2} transparent toneMapped={false} opacity={0.9} />
+             <Edges color="rgb(200, 60, 200)"></Edges>
+            </>
+            : <meshStandardMaterial transparent opacity={0.1} />}
             <boxGeometry />
         </mesh>
     );
 }
 export default Cell;
+
+
+//add click handler to cell component
+//must be defined in parent component and passed down as prop without being called
+//git push origin threelayer
+//not working
+//<Bloom mipmapBlur luminanceThreshold={1} mipmapBlur />
