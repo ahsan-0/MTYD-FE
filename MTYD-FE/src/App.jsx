40cb0d6d3098b009711dd909ff2f5b8f0313bb3e
3782291211 <Laklouk98@hotmail.com>
2023-01-29 13:52:43 +0000

    Adding route paths to App component

--- MTYD-FE/src/App.jsx
+++ MTYD-FE/src/App.jsx
@@ -2,17 +2,19 @@ import "./App.css";
 import ThreeDimensionalGame from "./components/ThreeDimensionalGame";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/Nav";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <Routes>
-        <Route path="/3dgame" element={<ThreeDimensionalGame />}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/3dgame" element={<ThreeDimensionalGame/>}/>
       </Routes>
     </div>
   );
-}
+};
 
 
 export default App;
