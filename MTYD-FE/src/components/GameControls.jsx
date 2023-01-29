6ac5012e3fb6d5131c6ba8f7610fcf816a5ce209
3782291211 <Laklouk98@hotmail.com>
2023-01-29 09:47:24 +0000

    Adding new component enabling user to control/modify the game.

---
+++ MTYD-FE/src/components/GameControls.jsx
@@ -0,0 +1,40 @@
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+const GameControls = ({setControls}) => {
+
+const handleClick = e => {
+  let click = 0;
+  if (!e.target.id) return;
+
+  if (e.target.id === "faster" || e.target.id === "slower") {
+    setControls({button: e.target.id, speedModifier: ++click});
+  } else {
+    setControls(prev => ({...prev, button: e.target.id}))
+  }
+};
+
+return (
+<section className="game-controls">
+<ButtonGroup onClick={handleClick} aria-label="Basic example">
+  <Button id="start" variant="secondary">Start</Button>
+  <Button id="stop" variant="secondary">Stop</Button>
+  <Button id="reset" variant="secondary">Reset</Button>
+  <Button id="faster" variant="secondary">🡱 faster</Button>
+  <Button id="slower" variant="secondary">🡳 slower</Button>
+  <DropdownButton as={ButtonGroup} title="Edge type" className="bg-nested-dropdown">
+   <Dropdown.Item id="edge" eventKey="1">Hard edge</Dropdown.Item>
+   <Dropdown.Item id="wrap" eventKey="2">Wrap around</Dropdown.Item>
+ </DropdownButton>
+ <DropdownButton as={ButtonGroup} title="Click to interact" className="bg-nested-dropdown">
+   <Dropdown.Item id="enableClick" eventKey="1">Enable</Dropdown.Item>
+   <Dropdown.Item id="disableClick" eventKey="2">Disable</Dropdown.Item>
+ </DropdownButton>
+ <Button id="clear" variant="secondary">Erase</Button>
+</ButtonGroup>
+</section>)
+};
+
+export default GameControls;
