d2fcaedc0cf5ef0a2e3893fab4ccfbb2fcb61f50
3782291211 <Laklouk98@hotmail.com>
2023-01-29 13:57:54 +0000

    Adding 3D text above table component

--- MTYD-FE/src/components/Table.jsx
+++ MTYD-FE/src/components/Table.jsx
@@ -1,27 +1,27 @@
 import img from "../assets/green.png";
 import * as THREE from "three";
-//import { useContext } from "react";
-//import { BoardConfigContext } from "../contexts/BoardConfigurationContext";
+import { Float, Text3D, useMatcapTexture } from "@react-three/drei";
+import retro from '../assets/retro.json';
 
 function Table({boardConfiguration}) {
   const texture = new THREE.TextureLoader().load(img);
-  //const { boardConfiguration } = useContext(BoardConfigContext);
   const boxLength = boardConfiguration;
   
+  function MatCap({texture}) {
+    const [matcap] = useMatcapTexture(texture, 256);
+    return <meshMatcapMaterial matcap={matcap} />
+  };
+  
   return (
-    <>
-      <mesh position={[boxLength / 2, -1, boxLength / 2]}
-      >
-        <meshMatcapMaterial
+    <><mesh position={[boxLength / 2, -1, boxLength / 2]}>
+      <meshMatcapMaterial
           opacity={1}
           depthTest={true}
           depthWrite={true}
           alphaTest={0}
           color={"white"}
-          matcap={texture}
-        />
-        <boxGeometry args={[boxLength + 3, 1, boxLength + 3]}
-        />
+          matcap={texture}/>
+        <boxGeometry args={[boxLength + 3, 1, boxLength + 3]}/>
       </mesh>
       <mesh position={[boxLength / 2, -0.2, -1]}>
       <meshMatcapMaterial
@@ -30,9 +30,8 @@ function Table({boardConfiguration}) {
           depthWrite={true}
           alphaTest={0}
           color={"white"}
-          matcap={texture}
-        />
-        <boxGeometry args={[boxLength + 1, 0.6, 1]} />
+          matcap={texture}/>
+        <boxGeometry args={[boxLength + 1, 0.6, 1]}/>
       </mesh>
      <mesh position={[boxLength / 2, -0.2, boxLength + 1]}>
         <meshMatcapMaterial
@@ -41,8 +40,7 @@ function Table({boardConfiguration}) {
           depthWrite={true}
           alphaTest={0}
           color={"white"}
-          matcap={texture}
-        />
+          matcap={texture}/>
         <boxGeometry args={[boxLength + 1, 0.6, 1]}/>
       </mesh>
 
@@ -53,8 +51,7 @@ function Table({boardConfiguration}) {
           depthWrite={true}
           alphaTest={0}
           color={"white"}
-          matcap={texture}
-        />
+          matcap={texture}/>
         <boxGeometry args={[1, 0.6, boxLength + 3
         ]}/>
       </mesh>
@@ -66,20 +63,27 @@ function Table({boardConfiguration}) {
           depthWrite={true}
           alphaTest={0}
           color={"white"}
-          matcap={texture}
-        />
+          matcap={texture}/>
         <boxGeometry args={[1, 0.6, boxLength + 3]}/>
       </mesh>
+
+      <mesh position = {[-4, 5, -5]}>
+      <Float speed={2.4} rotationIntensity={2} floatIntensity={2} floatingRange={[0, 1]}>
+      <Text3D 
+      font={retro}
+      height={0.5}
+      letterSpacing={-0.06}
+      size={2.8}
+      curveSegments={10}
+      bevelEnabled
+      bevelThickness={0.02}
+      bevelOffset={0}
+      bevelSegments={5}>AUTOMATRIX<MatCap texture={'422509_C89536_824512_0A0604'}/></Text3D>
+      </Float></mesh>
     </>
   );
 }
 export default Table;
-
-//  <boxGeometry args={[length, width, height]}/>
-
-//turn the table into a flat plane
-//set the table size to be the same as all of the cells
-
 
 
 /*
@@ -101,5 +105,5 @@ export default Table;
     setBoxLength(cellCoords[cellCoords.length - 1].coords[0]);
   },[]);
 
- // const boxLength = size[size.length - 1].coords[0]
+  const boxLength = size[size.length - 1].coords[0]
 */
