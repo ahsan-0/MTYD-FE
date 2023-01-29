e2940a8165227b2c79d6c072f57bae97ef19ea20
3782291211 <Laklouk98@hotmail.com>
2023-01-29 09:48:39 +0000

    Integrating control UI with game logic.

--- MTYD-FE/src/components/PlayArea.jsx
+++ MTYD-FE/src/components/PlayArea.jsx
@@ -1,9 +1,6 @@
-import Table from "./Table";
 import Cell from "./Cell";
-//import { boardData } from "./board.json";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
-import { useEffect } from "react";
 
 const boardString2 = "000110000111011 101110110100111 100011000101111 110011110011111 001001110000111 011101011101111 011011111010111 01100111001011 111011111110111 001111011101111 01110100111111 011010000110111 011111010001011 111011011110111 001010011101111";
 const boardString1 = "111111111111111 ".repeat(15);
@@ -11,40 +8,49 @@ const boardString = "000000000000000 000000000000000 000000000000000 00000000000
 const boardArray = (str => str.split(" ").map(m => m.split("").map(m => +m)))(boardString);
 
 
-function PlayArea() {
+function PlayArea({setBoardConfiguration, controls}) {
   const [gameIsRunning, setGameIsRunning] = useState(false);
   const [gameGrid, setGameGrid] = useState(() => boardArray);
-  //const [boardConfiguration, setBoardConfiguration] = useState([{coords: [0, 0, 0], alive: 1}]);
- 
+  const [interval, setInterval] = useState(500);
+  const [edgeCondition, setEdgeCondition] = useState('wrap');
+  const [enableInteract, setEnableInteract] = useState(true);
+
   const gameRef = useRef(gameIsRunning);
   gameRef.current = gameIsRunning;
-
-  /*useEffect(() => {
-    const cellCoords = [];
-    for (let i = 0; i < boardArray.length; i++) {
-      for (let j = 0; j < boardArray[i].length; j++) {
-        cellCoords.push({ coords: [i, 0, j], alive: boardArray[i][j] });
-      }
+  
+  useEffect(() => {
+    setBoardConfiguration(gameGrid.length - 1);
+  }, []);
+  
+  useEffect(() => {
+  const {button} = controls;
+  console.log(button);
+    if (button === "start") {
+      setGameIsRunning(true);
+      gameRef.current = true;
+      runGame();
+    } else if (button === "stop") {
+      setGameIsRunning(false);
+      gameRef.current = false;
+    } else if (button === "faster" && interval > 120) {
+      setInterval(prev => prev - 100);
+    } else if (button === "slower") {
+      setInterval(prev => prev + 100);
+    } else if (button === "reset") {
+      setGameIsRunning(false);
+      gameRef.current = false;
+      setGameGrid(boardArray);
+    } else if (button === "edge") {
+      setEdgeCondition("edge");
+    } else if (button === "wrap") {
+      setEdgeCondition("wrap");
+    } else if (button === "enableClick") {
+      setEnableInteract(true);
+    } else if (button === "disableClick") {
+      setEnableInteract(false);
     }
-    setBoardConfiguration(cellCoords);
-  },[]);*/
+  }, [controls]);
 
-  function Box() {
-    return (
-    <mesh 
-      onClick={() => {
-      setGameIsRunning(!gameIsRunning);
-      if (!gameIsRunning) {
-        gameRef.current = true;
-        runGame();
-      };
-      }} 
-      position={[-4, 2, 0]}>
-      <boxGeometry attach="geometry"/>
-      <meshLambertMaterial attach="material" color="hotpink" />
-    </mesh>
-   )
-  }
 
   const coordOffset = [[0, 1], [0, -1], [1, -1], [-1, 1], [1, 1], [-1, -1], [1, 0], [-1, 0]];
 
@@ -56,18 +62,18 @@ function PlayArea() {
       for (let j = 0; j < gameGrid[i].length ; j++) {
         let liveNeighbours = 0;
         coordOffset.forEach(([x, y]) => {
-          /* lines 60-65 define boundary conditions and disallow wrap-around functionality
-          if ( i + x >= 0 
-            && i + x < gameGrid.length 
-            && j + y >= 0 
-            && j + y < gameGrid[i].length) {
-              liveNeighbours += gameGrid[i + x][j + y];
-          }*/
-
-          // lines 68-70 enable cells to wrap around to other side if offset coordinates are -ve
+          if (edgeCondition === "edge") {
+             if ( i + x >= 0 
+             && i + x < gameGrid.length 
+             && j + y >= 0 
+             && j + y < gameGrid[i].length) {
+               liveNeighbours += gameGrid[i + x][j + y];
+             }
+          } else if (edgeCondition === "wrap") {
           const xWrapAroundOffset = (i + x + gameGrid[i].length) % gameGrid[i].length;
           const yWrapAroundOffset = (j + y + gameGrid.length) % gameGrid.length;
-          liveNeighbours += gameGrid[xWrapAroundOffset][yWrapAroundOffset]
+          liveNeighbours += gameGrid[xWrapAroundOffset][yWrapAroundOffset];
+          };
         });
         if (liveNeighbours < 2 || liveNeighbours > 3) {
           newGameGrid[i][j] = 0;
@@ -76,7 +82,6 @@ function PlayArea() {
         }
       }
     };
-
     setGameGrid(newGameGrid);
   };
 
@@ -91,19 +96,16 @@ function PlayArea() {
   };
 
   useEffect(() => {
-    setTimeout(runGame, 1000);
+    setTimeout(runGame, interval);
   }, [gameGrid]);
 
   return (
   <>
-    <Box/>
-
-    
-    {boardConfig(gameGrid).map((cell) => (
-    <Cell key={uuidv4()} position={cell.coords} living={cell.alive}/>
+    {boardConfig(gameGrid).map(cell => (
+    <Cell key={uuidv4()} position={cell.coords} living={cell.alive} interact={enableInteract}/>
     ))}
-    <Table size={boardConfig(gameGrid)} />
   </>
   );
 }
 export default PlayArea;
+
