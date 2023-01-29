9c452f2190f73b593debd32181390dc34c840523
ahsan-0 <ashanb995@gmail>
2023-01-29 19:26:01 +0000

    make cols and rows dynamic with useState and pass props to components

--- MTYD-FE/src/App.jsx
+++ MTYD-FE/src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useCallback, useRef } from "react";
+import { Routes, Route } from "react-router-dom";
 import produce from "immer";
+import GenerateBoard from "./components/GenerateBoard";
+import Patterns from "./components/Patterns";
+import Tutorial from "./components/Tutorial";
+import Nav from "./components/Nav";
 import DefaultBoard from "./components/DefaultBoard";
-const numRows = 35;
-const numCols = 50;
 const operations = [
   [0, 1],
   [0, -1],
@@ -13,34 +16,34 @@ const operations = [
   [1, 0],
   [-1, 0],
 ];
-const generateEmptyGrid = () => {
-  const rows = [];
-  for (let i = 0; i < numRows; i++) {
-    rows.push(Array.from(Array(numCols), () => 0));
-  }
-  return rows;
-};
-
 const App = () => {
   const [running, setRunning] = useState(false);
-  const [grid, setGrid] = useState(() => {
-    return generateEmptyGrid();
-  });
+  const [rows, setRows] = useState(0);
+  const [cols, setCols] = useState(0);
+  const [generated, setGenerated] = useState(false);
+  const [grid, setGrid] = useState([]);
   const runningRef = useRef(running);
   runningRef.current = running;
+  const generateEmptyGrid = () => {
+    const arrRows = [];
+    for (let i = 0; i < rows; i++) {
+      arrRows.push(Array.from(Array(cols), () => {}));
+    }
+    return arrRows;
+  };
   const runSimulation = useCallback(() => {
     if (!runningRef.current) {
       return;
     }
     setGrid((g) => {
       return produce(g, (gridCopy) => {
-        for (let i = 0; i < numRows; i++) {
-          for (let k = 0; k < numCols; k++) {
+        for (let i = 0; i < rows; i++) {
+          for (let k = 0; k < cols; k++) {
             let neighbors = 0;
             operations.forEach(([x, y]) => {
               const newI = i + x;
               const newK = k + y;
-              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+              if (newI >= 0 && newI < rows && newK >= 0 && newK < cols) {
                 neighbors += g[newI][newK];
               }
             });
@@ -56,17 +59,51 @@ const App = () => {
     setTimeout(runSimulation, 300);
   }, []);
   return (
-    <DefaultBoard
-      running={running}
-      setRunning={setRunning}
-      setGrid={setGrid}
-      grid={grid}
-      numCols={numCols}
-      numRows={numRows}
-      generateEmptyGrid={generateEmptyGrid}
-      runningRef={runningRef}
-      runSimulation={runSimulation}
-    />
+    <>
+      <Nav />
+      <Routes>
+        <Route
+          path="/automatrix"
+          element={
+            <GenerateBoard
+              setCols={setCols}
+              setRows={setRows}
+              setGenerated={setGenerated}
+              generated={generated}
+              generateEmptyGrid={generateEmptyGrid}
+              setGrid={setGrid}
+            />
+          }
+        />
+        <Route
+          path="/patterns"
+          element={
+            <Patterns
+              running={running}
+              setRunning={setRunning}
+              setGrid={setGrid}
+              grid={grid}
+              cols={cols}
+              rows={rows}
+              runningRef={runningRef}
+              runSimulation={runSimulation}
+            />
+          }
+        />
+        <Route path="/how-to-play" element={<Tutorial />} />
+      </Routes>
+      <DefaultBoard
+        running={running}
+        setRunning={setRunning}
+        setGrid={setGrid}
+        grid={grid}
+        cols={cols}
+        rows={rows}
+        generateEmptyGrid={generateEmptyGrid}
+        runningRef={runningRef}
+        runSimulation={runSimulation}
+      />
+    </>
   );
 };
 export default App;
