2c1c3c4cda3af32a20f874c0bf90fdcba0944bb1
3782291211 <Laklouk98@hotmail.com>
2023-01-29 13:54:39 +0000

    Removing comments

--- MTYD-FE/src/components/Cell.jsx
+++ MTYD-FE/src/components/Cell.jsx
@@ -1,4 +1,3 @@
-//import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import { Edges, Plane } from "@react-three/drei"
 import { Physics } from "@react-three/cannon";
 import { useState } from "react";
@@ -12,7 +11,6 @@ function Cell({ position, living, interact }) {
               setIsAlive(prev => !prev);
             };
         } } position={position}>
-            <Physics>
             {isAlive ? <>
             <meshStandardMaterial emissive="hotpink" emissiveIntensity={2} transparent toneMapped={false} opacity={0.9} />
             <Edges color="rgb(200, 60, 200)"/>
@@ -20,15 +18,7 @@ function Cell({ position, living, interact }) {
             <><mesh position={[0, -0.5, 0]}>
             <meshStandardMaterial  color="pink" transparent opacity={0.6}/>
             <boxGeometry args={[1, 0.05, 1]}/></mesh></>}
-            </Physics>
         </mesh>
     );
 }
 export default Cell;
-
-
-//add click handler to cell component
-//must be defined in parent component and passed down as prop without being called
-//git push origin threelayer
-
-//not working - <Bloom mipmapBlur luminanceThreshold={1} mipmapBlur />
