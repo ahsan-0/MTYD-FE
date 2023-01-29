cf2e5661e9cf2952fb44650ba7aebcc080ea0877
ahsan-0 <ashanb995@gmail>
2023-01-29 19:29:20 +0000

    create tutorial component to show rules of the game

---
+++ MTYD-FE/src/components/Tutorial.jsx
@@ -0,0 +1,17 @@
+function Tutorial() {
+  return (
+    <div className="tutorial">
+      <h1>Tutorial</h1>
+      <h4>Loneliness: A cell with less than 2 adjoining neighbors dies.</h4>
+      <img src="../../public/ruleOne.png" alt="rule one" />
+      <h4>OverCrowding: A cell with more than three adjoining cells dies.</h4>
+      <img src="../../public/ruleTwo.png" alt="rule two" />
+      <h4>Reproduction: An empty cell with more than three adjoining cells comes alive.</h4>
+      <img src="../../public/ruleThree.png" alt="rule three" />
+      <h4>Stasis: A cell with exactly two adjoining cells remains the same</h4>
+      <img src="../../public/ruleFour.png" alt="rule four" />
+    </div>
+  );
+}
+
+export default Tutorial;
