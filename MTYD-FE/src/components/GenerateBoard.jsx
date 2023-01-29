2fad2d76f0f2074d01879cf413d77749b16e5203
ahsan-0 <ashanb995@gmail>
2023-01-29 19:27:04 +0000

    create Generate board component that allows user to make any size board

---
+++ MTYD-FE/src/components/GenerateBoard.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function GenerateBoard({ setRows, setCols, setGenerated, setGrid, generateEmptyGrid }) {
+  const [rowInput, setRowInput] = useState(0);
+  const [colInput, setColInput] = useState(0);
+  const handleRowChange = (event) => {
+    setRowInput(parseInt(event.target.value));
+  };
+  const handleColChange = (event) => {
+    setColInput(parseInt(event.target.value));
+  };
+  return (
+    <div>
+      <input type="number" placeholder="rows" onChange={handleRowChange}></input>
+      <input type="number" placeholder="columns" onChange={handleColChange}></input>
+      <button
+        onClick={() => {
+          setRows(rowInput);
+          setCols(colInput);
+          setGrid(generateEmptyGrid());
+          setGenerated(true)
+        }}
+      >
+        Generate Board
+      </button>
+    </div>
+  );
+}
+
+export default GenerateBoard;
