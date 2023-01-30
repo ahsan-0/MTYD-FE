43733babe69b1c9652abc8f5e59821f9c070b486
ahsan-0 <ashanb995@gmail>
2023-01-30 16:43:28 +0000

    make a new request

--- MTYD-FE/src/api.js
+++ MTYD-FE/src/api.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export const getPatterns = () => {
   return axios.get("https://automatrix-app.azurewebsites.net/api/patterns");
 };
+
+export const getUsers = () => {
+  return axios.get("https://automatrix-app.azurewebsites.net/api/users");
+}
