47995debc4a91583514eeabbc2d76e8665befaea
3782291211 <Laklouk98@hotmail.com>
2023-01-31 02:06:18 +0000

    Removing references to deleted files

--- MTYD-FE/src/components/Home.jsx
+++ MTYD-FE/src/components/Home.jsx
@@ -1,7 +1,7 @@
-import { CameraControls, Center, Environment, Float, OrbitControls, PerspectiveCamera, Text3D, useMatcapTexture } from "@react-three/drei";
+import { Center, Environment, Float, OrbitControls, PerspectiveCamera, Text3D, useMatcapTexture } from "@react-three/drei";
 import { Canvas, useThree } from "@react-three/fiber";
 import retro from '../assets/retro.json';
-import { CubeTextureLoader } from 'three';
+import { BoxGeometry, CubeTextureLoader } from 'three';
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
 
 const Home = () => {
@@ -10,14 +10,7 @@ function Background() {
   const { scene } = useThree();
   const loader = new CubeTextureLoader();
   // The CubeTextureLoader load method takes an array of urls representing all 6 sides of the cube.
-  const texture = loader.load([
-    '/everspace/ev1.jpg',
-    '/everspace/ev2.jpg',
-    '/everspace/ev3.jpg',
-    '/everspace/ev4.jpg',
-    '/everspace/ev5.jpg',
-    '/everspace/ev6.jpg'
-  ]);
+  const texture = loader.load([]);
   //try out userLoader hook
 
   // Set the scene background property to the resulting texture.
@@ -36,9 +29,8 @@ return (
     <EffectComposer>
         <Bloom mipmapBlur luminanceThreshold={1}></Bloom>
     </EffectComposer>
-    <Environment background={true} blur={0} files={['everspace/d1.jpg', 'everspace/d2.jpg', 'everspace/d3.jpg', 'everspace/d4.jpg', 'everspace/d5.jpg', 'everspace/d6.jpg']} path="/"/>
-      <PerspectiveCamera makeDefault fov={75} position={[10, 10, 10]} />
-      <CameraControls />
+      <PerspectiveCamera makeDefault fov={70} position={[0, 20, 10]} />
+      <OrbitControls target={[0, 0, 0]} maxPolarAngle={Math.PI / 2}/>
     
       <ambientLight intensity={0.8} />
       <Float speed={2.4} rotationIntensity={2.4} floatIntensity={2} floatingRange={[-2, 2]}>
@@ -67,7 +59,6 @@ return (
       bevelOffset={0}
       bevelSegments={5}
       > Multiply or die.<MatCap texture={'3B3C3F_DAD9D5_929290_ABACA8'} /> </Text3D></mesh>
-
       </Center>
       </Float>
 </Canvas>
