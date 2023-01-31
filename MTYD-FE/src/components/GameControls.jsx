479ddcf19ce62d066c7eea477bf2b097d736ec62
3782291211 <Laklouk98@hotmail.com>
2023-01-31 02:06:00 +0000

    Adding disable condition to buttons

--- MTYD-FE/src/components/GameControls.jsx
+++ MTYD-FE/src/components/GameControls.jsx
@@ -7,11 +7,11 @@ import { GameControlsContext } from '../contexts/GameControlsContext';
 
 const GameControls = () => {
 
-const { setControls } = useContext(GameControlsContext);
+const { controls, setControls } = useContext(GameControlsContext);
 
 const handleClick = e => {
   let click = 0;
-  if (!e.target.id) return;
+  if (!e.target.id || e.target.id === 'physics') return;
 
   if (e.target.id === "faster" || e.target.id === "slower") {
     setControls({button: e.target.id, speedModifier: ++click});
@@ -20,23 +20,28 @@ const handleClick = e => {
   }
 };
 
+const condition = controls.button === "enablePhysics";
+
 return (
 <section className="game-controls">
 <ButtonGroup onClick={handleClick} aria-label="Basic example">
-  <Button id="start" variant="secondary">Start</Button>
-  <Button id="stop" variant="secondary">Stop</Button>
-  <Button id="reset" variant="secondary">Reset</Button>
-  <Button id="faster" variant="secondary">🡱 faster</Button>
-  <Button id="slower" variant="secondary">🡳 slower</Button>
-  <DropdownButton as={ButtonGroup} title="Edge type" className="bg-nested-dropdown">
+  <Button disabled={condition} id="start" variant="secondary">Start</Button>
+  <Button disabled={condition} id="stop" variant="secondary">Stop</Button>
+  <Button disabled={condition} id="reset" variant="secondary">Reset</Button>
+  <Button disabled={condition} id="faster" variant="secondary">🡱 faster</Button>
+  <Button disabled={condition} id="slower" variant="secondary">🡳 slower</Button>
+  <DropdownButton disabled={condition} as={ButtonGroup} title="Edge type" className="bg-nested-dropdown">
    <Dropdown.Item id="edge" eventKey="1">Hard edge</Dropdown.Item>
    <Dropdown.Item id="wrap" eventKey="2">Wrap around</Dropdown.Item>
  </DropdownButton>
- <DropdownButton as={ButtonGroup} title="Click to interact" className="bg-nested-dropdown">
+ <DropdownButton disabled={condition} as={ButtonGroup} title="Click to interact" className="bg-nested-dropdown">
    <Dropdown.Item id="enableClick" eventKey="1">Enable</Dropdown.Item>
    <Dropdown.Item id="disableClick" eventKey="2">Disable</Dropdown.Item>
  </DropdownButton>
- <Button id="clear" variant="secondary">Erase</Button>
+ <DropdownButton as={ButtonGroup} id="physics" title="Physics" className="bg-nested-dropdown">
+   <Dropdown.Item id="enablePhysics" eventKey="1">Enable</Dropdown.Item>
+   <Dropdown.Item id="disablePhysics" eventKey="2">Disable</Dropdown.Item>
+ </DropdownButton>
 </ButtonGroup>
 </section>)
 };
