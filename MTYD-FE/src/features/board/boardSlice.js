3428d634f3679739b7de8bd076f79253e22d6ecd
3782291211 <Laklouk98@hotmail.com>
2023-01-31 17:51:31 +0000

    Transferring state

---
+++ MTYD-FE/src/features/board/boardSlice.js
@@ -0,0 +1,84 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const boardSlice = createSlice({
+  name: 'board',
+  initialState: {
+    configuration: [[0,0,0,0,0],[0,0,0,0,0],[0,1,0,0,0],[0,1,0,0,0],[0,1,0,0,0]],
+    running: false,
+    wrap: true,
+    interval: 1000,
+  },
+  reducers: {
+    nextBoard: state => {
+    const nextBoard = [...state.configuration]
+    const nextBoard2 = []
+    for (let i = 0; i < nextBoard.length; i++) {
+      nextBoard2.push([])
+      for (let j = 0; j < nextBoard[i].length; j++) {
+        nextBoard2[i].push(0)
+        let liveNeighbours = 0;
+        coordOffset.forEach(([x, y]) => {
+          if (state.wrap === false) {
+            if (
+              i + x >= 0 &&
+              i + x < nextBoard.length &&
+              j + y >= 0 &&
+              j + y < nextBoard[i].length
+            ) {
+              liveNeighbours += nextBoard[i + x][j + y];
+            }
+          } else if (state.wrap === true) {
+            const xWrapAroundOffset =
+              (i + x + nextBoard[i].length) % nextBoard[i].length;
+            const yWrapAroundOffset =
+              (j + y + nextBoard.length) % nextBoard.length;
+            liveNeighbours += nextBoard[xWrapAroundOffset][yWrapAroundOffset];
+          }
+        });
+        if (liveNeighbours < 2 || liveNeighbours > 3) {
+          nextBoard2[i][j] = 0;
+        } else if (nextBoard[i][j] === 0 && liveNeighbours === 3) {
+          nextBoard2[i][j] = 1;
+        }else if (nextBoard[i][j] === 1 && liveNeighbours === 2 ) {
+          nextBoard2[i][j] = 1;
+        }
+
+      }
+    }
+ 
+    return {
+      ...state, configuration: nextBoard2,
+    }
+
+
+  },
+  flipRunning: state => {
+    return {...state, running: !state.running}
+  },
+  flipWrap: state => {
+    return {...state, wrap: !state.wrap}
+  },
+  increaseSpeed: state => {
+    return {...state, interval: !state.interval + 150}
+  },
+  decreaseSpeed: state => {
+    return {...state, interval: !state.interval - 150}
+  }
+  }
+})
+
+// Action creators are generated for each case reducer function
+export const {nextBoard, flipRunning, flipWrap, increaseSpeed, decreaseSpeed} = boardSlice.actions
+
+export default boardSlice.reducer
+
+  const coordOffset = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0],
+  ];
