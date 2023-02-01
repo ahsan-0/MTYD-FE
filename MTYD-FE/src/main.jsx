f46a21368956d36ac714b151cd8507cd88e28d5e
3782291211 <Laklouk98@hotmail.com>
2023-02-01 00:37:39 +0000

    Adding imports

--- MTYD-FE/src/main.jsx
+++ MTYD-FE/src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-//import 'bootstrap';
-//import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from 'react-router-dom';
 import { GameControlsProvider } from './contexts/GameControlsContext';
 import { Provider } from 'react-redux';
-import store from './app/store'
+import store from './app/store';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
