056078cdeb7cd19512b3f5444f2647ae25beca3c
3782291211 <Laklouk98@hotmail.com>
2023-02-02 04:38:52 +0000

    Refactoring code section.

--- MTYD-FE/src/components/CellTwo.jsx
+++ MTYD-FE/src/components/CellTwo.jsx
@@ -2,24 +2,43 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useDispatch, useSelector } from "react-redux";
 import { updateBoard } from "../features/board/boardSlice";
+import { useLocation } from 'react-router-dom';
+import { Edges } from "@react-three/drei";
 
-
-function CellTwo({ uidv4, alive, location  }) {
-  const board = useSelector((state) => state.board) 
-  const dispatch = useDispatch()
-  const message = "hello i am a pear"
-
-  const greeting = msg => msg;
-
+function CellTwo({ alive, location, position }) {
+  //const board = useSelector((state) => state.board) ;
+  const dispatch = useDispatch();
+  const url = useLocation();
+  
   function handleClick(args) {
     return () => {
       dispatch(updateBoard(args));
     };
+  };
+
+  if (url.pathname[1] === "3") {
+  return(<mesh onClick={handleClick(location)} position={position}>
+    {alive ? <>
+    <meshStandardMaterial emissive="hotpink" emissiveIntensity={2} transparent toneMapped={false} opacity={0.9} />
+    <Edges color="rgb(200, 60, 200)"/>
+    <boxGeometry/></> :
+    <><mesh position={[0, -0.5, 0]}>
+    <meshStandardMaterial  color="pink" transparent opacity={0.6}/>
+    <boxGeometry args={[1, 0.05, 1]}/></mesh></>}
+  </mesh>);
+  } else {
+    return (<div key={uuidv4()} className={alive === 1 ? "cellgridliving" : "cellgriddead"} onClick={handleClick(location)}/>)
   }
-
-
-  return (alive === 1 ? <div key={uuidv4()} className={"cellgridliving"} onClick={handleClick(location)}></div> : <div key={uuidv4()} onClick={handleClick(location)} className={"cellgriddead"}></div>  
-
-  );
 }
+
 export default CellTwo;
+
+
+/*
+if (alive === 1) {
+      return (<div key={uuidv4()} className={"cellgridliving"} onClick={handleClick(location)}></div>)
+    } else {
+      return (<div key={uuidv4()} onClick={handleClick(location)} className={"cellgriddead"}></div>)
+    };
+
+*/
