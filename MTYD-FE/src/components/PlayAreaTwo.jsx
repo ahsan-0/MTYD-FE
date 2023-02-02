218f26fcbb79411ca8d7452a6f7cd68a5c40e419
3782291211 <Laklouk98@hotmail.com>
2023-02-02 04:41:41 +0000

    Adding game control logic to 2D board component.

--- MTYD-FE/src/components/PlayAreaTwo.jsx
+++ MTYD-FE/src/components/PlayAreaTwo.jsx
@@ -8,6 +8,10 @@ import {
   decreaseSpeed,
   increaseBoard,
   decreaseBoard,
+  resetBoard,
+  randomiseBoard,
+  hideTitle,
+  clearTable
 } from "../features/board/boardSlice";
 import { useEffect, useContext } from "react";
 import { GameControlsContext } from "../contexts/GameControlsContext";
@@ -16,7 +20,87 @@ import CellTwo from "./CellTwo.jsx";
 
 function PlayAreaTwo() {
   const { controls } = useContext(GameControlsContext);
+
   useEffect(() => {
+    const { button } = controls;
+    if (button === "start") {
+      dispatch(flipRunning());
+      dispatch(nextBoard());
+    } else if (button === "stop") {
+      dispatch(flipRunning());
+    } else if (button === "incSize") {
+      dispatch(increaseBoard());
+    } else if (button === "decSize") {
+      dispatch(decreaseBoard());
+    } else if (button === "faster" && board.interval > 120) {
+      dispatch(increaseSpeed());
+    } else if (button === "slower") {
+      dispatch(decreaseSpeed());
+    } else if (button === "reset") {
+      dispatch(resetBoard());
+      if (board.running) {
+        dispatch(flipRunning());
+      }
+    } else if (button === "randomise") {
+      dispatch(randomiseBoard());
+    } else if (button === "edge") {
+      dispatch(flipWrap());
+    } else if (button === "wrap") {
+      dispatch(flipWrap());
+    } else if (button === "enableClick") {
+      setEnableInteract(true);
+    } else if (button === "disableClick") {
+      setEnableInteract();
+    } else if (button === "hide") {
+      dispatch(hideTitle());
+    } else if (button === "clear") {
+      dispatch(clearTable());
+    }
+  }, [controls]);
+
+
+  const board = useSelector((state) => state.board);
+  const dispatch = useDispatch();
+  const grid = "1fr ";
+  const gridcolumns = grid.repeat(board.configuration.length);
+
+  function grimReaper(cell) {
+    if (cell === 1) {
+      return "cellgridliving";
+    }
+    return "cellgriddead";
+  }
+
+  useEffect(() => {
+    if (board.running) {
+      setTimeout(() => dispatch(nextBoard()), board.interval);
+    }
+  }, [board.configuration]);
+
+
+
+  return (
+    <>
+     <h1 className="tutorial_h1">Game Of Life in 2D</h1>
+      <div className="cellboard" style={{ gridTemplateColumns: gridcolumns }}>
+        {board.configuration.map((row, i) => {
+          return row.map((cell, k) => {
+            return <CellTwo location={[i,k]} key={uuidv4()} alive={cell} />;
+          });
+        })}
+      </div>
+      <GameControls />
+    </>
+  );
+}
+
+export default PlayAreaTwo;
+
+
+  
+/*
+
+useEffect(() => {
     const { button } = controls;
     if (button === "start") {
       dispatch(flipRunning());
@@ -43,41 +127,5 @@ function PlayAreaTwo() {
       setEnableInteract(false);
     }
   }, [controls]);
-  const board = useSelector((state) => state.board);
-  const dispatch = useDispatch();
-  const grid = "1fr ";
-  const gridcolumns = grid.repeat(board.configuration.length);
 
-  function grimReaper(cell) {
-    if (cell === 1) {
-      return "cellgridliving";
-    }
-    return "cellgriddead";
-  }
-
-  useEffect(() => {
-    if (board.running) {
-      setTimeout(() => dispatch(nextBoard()), board.interval);
-    }
-  }, [board.configuration]);
-
-
-
-  return (
-    <>
-      <div className="cellboard" style={{ gridTemplateColumns: gridcolumns }}>
-        {board.configuration.map((row, i) => {
-          return row.map((cell, k) => {
-            return <CellTwo location={[i,k]} key={uuidv4()} alive={cell} />;
-          });
-        })}
-      </div>
-      <GameControls />
-    </>
-  );
-}
-
-export default PlayAreaTwo;
-
-
-  
+*/
