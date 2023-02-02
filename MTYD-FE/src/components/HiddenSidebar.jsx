77182d8a8faa564bdd1b14676cd114d60851be22
ahsan-0 <ashanb995@gmail>
2023-02-02 14:44:20 +0000

    add images

--- MTYD-FE/src/components/HiddenSidebar.jsx
+++ MTYD-FE/src/components/HiddenSidebar.jsx
@@ -1,20 +1,24 @@
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import Offcanvas from "react-bootstrap/Offcanvas";
 
-function HiddenSidebar({showSidebar, setShowSidebar}) {
-
- const handleClose = () => setShowSidebar(false);
+function HiddenSidebar({ showSidebar, setShowSidebar }) {
+  const handleClose = () => setShowSidebar(false);
 
   return (
-    <> <Offcanvas show={showSidebar} onHide={handleClose}>
+    <>
+      <Offcanvas show={showSidebar} onHide={handleClose}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Tips and tricks</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           Multiply...or die. Is it a game or is it reality? Are you an automaton or the game master?
+          <h5>Example Patterns </h5>
+          <img className="gametipsImg" src="/pattern1.png" alt="pattern example 1" />
+          <img className="gametipsImg" src="/pattern3.png" alt="pattern example 3" />
+          <img className="gametipsImg" src="/pattern4.png" alt="pattern example 4" />
         </Offcanvas.Body>
       </Offcanvas>
     </>
   );
-};
+}
 
 export default HiddenSidebar;
